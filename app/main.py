95d04570cd6af916d3e5a4776d71010633fa2e74
Jayaditya Dev <jayadityadev261204@gmail.com>
2026-01-29 20:34:33 +0530

    added CORS functionality

--- app/main.py
+++ app/main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
-from .database import engine, Base
+from fastapi.middleware.cors import CORSMiddleware
+# from .database import engine, Base
 from .routers import post, user, auth, vote
 
 # Base.metadata.create_all(bind=engine)
 
+origins = [
+    "*"
+]
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
